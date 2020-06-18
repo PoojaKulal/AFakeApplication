68a2cbe484a13bedd8ac0818bf6202db580d8d62
Pooja <poojaps.kulal@gmail.com>
2020-06-19 00:22:04 +0530

    changed

--- multiplication.c
+++ multiplication.c
@@ -1,15 +1,1 @@
 #include<stdio.h>
-int result;
-int main()
-{
-    printf("Enter the two numbers");
-    scanf("%d %d",&a,&b);
-    multiply(a,b);
-   printf("%d",result);
-}
-int multiply(int a,int b)
-{
-    result=multiply(a*b);
-    return result;
-    
-}
